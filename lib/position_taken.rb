394449931384ca011d60d8ee178282314e6176e3
desmond beramendi <desmond7187@gmail.com>
2018-01-12 20:11:25 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, indx)
-  if (board[indx] != "  ")
+  if (board[indx] == "X" || board[indx] == "O")  
     false 
-  elsif (board[indx] == "X" || board[indx] == "O")
+  elsif (board[indx] != "  ")
     true 
   end 
 end 
